d313d303e44feb79c771f04660cc7618c709b347
hughwang10 <hughwang10@gmail.com>
2018-10-29 11:35:10 +0000

    update

--- run.sh
+++ run.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
-# docker run \
-#   --name jupyter_basic \
-#   -e JUPYTER_ENABLE_LAB=no \
-#   -v `pwd`:/home/jovyan/work \
-#   -p 8888:8888 \
-#   --rm \
-#   jupyter/minimal-notebook
-
 
 docker run \
   --rm \
   -p 8888:8888 \
   -e JUPYTER_ENABLE_LAB=no \
   -v /home/hugh/jupyterlab:/home/jovyan/work \
-  jupyter/datascience-notebook
+  jupyter/scipy-notebook
 
-#!/bin/bash
-#docker run -it \
-#-v `pwd`:/project \
-#--rm --name afg-tools \
-#--network host \
-#hugh/ansible_docker \
-#$1
+#datascience-notebook
+#minimal-notebook
